d81b6c5ca107fc7057200775dc43f31520369d85
lhrbuxiaoxin <lihaoran228@163.com>
2020-09-28 23:53:50 +0800

    nothing

--- LabCMS.EquipmentDomain.Server/Startup.cs
+++ LabCMS.EquipmentDomain.Server/Startup.cs
@@ -28,7 +28,6 @@ namespace LabCMS.EquipmentDomain.Server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers().AddJsonOptions(options=>
                 options.JsonSerializerOptions.PropertyNamingPolicy=null);
             services.AddSwaggerGen(c =>
